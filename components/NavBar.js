b4236d4a726db34356b13da899443406c7bbb406
sdfwefk11 <mknhsad3398@naver.com>
2023-08-28 23:07:21 +0900

    Style JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,49 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        href="/"
-      >
+      <Link className={router.pathname === "/" ? "active" : ""} href="/">
         Home
       </Link>
       <Link
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
+        className={router.pathname === "/about" ? "active" : ""}
         href="/about"
       >
         About
       </Link>
+      <style jsx global>{`
+        nav {
+          background-color: black;
+          border-radius: 5px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+          :hover {
+            background-color: white;
+            color: black;
+            border-radius: 5px;
+          }
+        }
+        a {
+          text-decoration: none;
+          color: gray;
+          margin: 10px 10px;
+          font-size: 20px;
+        }
+        .active {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          color: white;
+          font-size: 30px;
+          background-color: blue;
+          border-radius: 5px;
+        }
+      `}</style>
     </nav>
   );
 }
