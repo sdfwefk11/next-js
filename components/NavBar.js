39b132e41374b0a04e1c5446f7c57e7ff655a6a7
sdfwefk11 <mknhsad3398@naver.com>
2023-08-31 23:25:18 +0900

    Movie data fetch

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,43 +5,43 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link className={router.pathname === "/" ? "active" : ""} href="/">
-        Home
-      </Link>
-      <Link
-        className={router.pathname === "/about" ? "active" : ""}
-        href="/about"
-      >
-        About
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link className={router.pathname === "/" ? "active" : ""} href="/">
+          Home
+        </Link>
+        <Link
+          className={router.pathname === "/about" ? "active" : ""}
+          href="/about"
+        >
+          About
+        </Link>
+      </div>
       <style jsx global>{`
         nav {
-          background-color: black;
-          border-radius: 5px;
           display: flex;
-          flex-direction: row;
-          justify-content: space-between;
+          gap: 10px;
+          flex-direction: column;
           align-items: center;
-          :hover {
-            background-color: white;
-            color: black;
-            border-radius: 5px;
-          }
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
-          color: gray;
-          margin: 10px 10px;
-          font-size: 20px;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
+          color: tomato;
+        }
+        nav div {
           display: flex;
-          justify-content: center;
-          align-items: center;
-          color: white;
-          font-size: 30px;
-          background-color: blue;
-          border-radius: 5px;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import HeaderTitle from "../../components/HeaderTitle";
 
+const API_KEY = "1b86acec5851a012fe6a11323ab396f0";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <HeaderTitle title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h1>Loading...</h1>}
+      {movies?.map((res) => (
+        <div key={res.id}>
+          <h1>{res.title}</h1>
+        </div>
+      ))}
     </div>
   );
 }
