92460e6d2604e597926b3ad61bf6007276fb536b
sdfwefk11 <mknhsad3398@naver.com>
2023-09-02 21:14:59 +0900

    Server side rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
 import HeaderTitle from "../../components/HeaderTitle";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <>
       <div className="container">
         <HeaderTitle title="Home" />
-        {!movies && <h1>Loading...</h1>}
-        {movies?.map((res) => (
+        {results?.map((res) => (
           <div className="movie" key={res.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${res.poster_path}`} />
-            <h1>{res.title}</h1>
+            <h4>{res.title}</h4>
           </div>
         ))}
       </div>
@@ -44,4 +35,15 @@ export default function Home() {
       `}</style>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
